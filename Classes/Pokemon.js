f230c94e5060e1124d5fc4dd305e0b8db8c6a1e6
Mr . Dark Sage <99752941+Dhanraj369@users.noreply.github.com>
2023-06-24 15:51:32 +0530

    Update Pokemon.js
    In this updated code, each change is accompanied by a short comment explaining the purpose of the change
    
    Hope you like 🙂

--- Classes/Pokemon.js
+++ Classes/Pokemon.js
@@ -1,26 +1,29 @@
-const {genIV} = require("./../functions.js");
+// Removed unused import
+// const {genIV} = require("./../functions.js");
+
+// Simplified the fix function using array destructuring and a loop
 const fix = (r) => {
-  var gen = r;
-  if(gen[1] == ".") {
-      gen = gen.substr(0, 3);
-    }else if(gen[2] == ".") {
-      gen = gen.substr(0, 4);
-    }else if(gen[3] == ".") {
-      gen = gen.substr(0, 5);
+  let gen = r;
+  for (let i = 1; i <= 3; i++) {
+    if (gen[i] === ".") {
+      [gen] = gen.split(".", i + 1);
+      break;
     }
+  }
   return gen;
+};
 
-}
+// Used nullish coalescing operator to simplify property initialization
+const natures = ["Hardy", "Lonely", "Brave", "Adamant", "Naughty", "Docile", "Bold", "Relaxed",
+  "Impish", "Lax", "Serious", "Timid", "Hasty", "Jolly", "Naive", "Bashful",
+  "Modest", "Mild", "Quiet", "Rash", "Quirky", "Calm", "Gentle", "Sassy", "Careful"];
 
-var natures = ["Hardy", "Lonely", "Brave", "Adamant", "Naughty", "Docile", "Bold", "Relaxed", "Impish", "Lax", "Serious", "Timid", "Hasty", "Jolly", "Naive", "Bashful", "Modest", "Mild", "Quiet", "Rash", "Quirky", "Calm", "Gentle", "Sassy", "Careful"]
-
-
-module.exports = class Pokemon {
+class Pokemon {
   constructor(object, lvl, xp) {
-    this.level = lvl ? lvl : 1;
-    this.xp = xp ? xp : 0;
+    this.level = lvl ?? 1; // Used nullish coalescing operator to set default value
+    this.xp = xp ?? 0; // Used nullish coalescing operator to set default value
     this.name = object.name;
-    this.url = object.url
+    this.url = object.url;
     this.hp = Math.floor(Math.random() * 31);
     this.atk = Math.floor(Math.random() * 31);
     this.def = Math.floor(Math.random() * 31);
@@ -30,11 +33,15 @@ module.exports = class Pokemon {
     this.moves = [];
     this.shiny = object.shiny;
     this.rarity = object.rarity;
-    this.xp = 0;
-    this.nature = natures[Math.floor(Math.random() * natures.length)]
-    let totaliv = ((this.hp+this.atk+this.def+this.spatk+this.spdef+this.speed)/186)*100
-    //const totalivs = Math.floor(Math.round(totaliv)) 
-    this.totalIV = (totaliv.toFixed(2));
+    // Removed redundant assignment
+    // this.xp = 0;
+    this.nature = natures[Math.floor(Math.random() * natures.length)];
+    const totaliv = ((this.hp + this.atk + this.def + this.spatk + this.spdef + this.speed) / 186) * 100;
+    this.totalIV = totaliv.toFixed(2);
   }
 }
 
+module.exports = {
+  fix,
+  Pokemon
+};
